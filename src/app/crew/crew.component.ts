ff92175d5869aa862bed70e6d51e46df6ed4cd0c
mkilp91 <mitchellakilpatrick@gmail.com>
2022-12-10 01:06:15 -0600

    completed studio (without bonus)

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { endTimeRange } from '@angular/core/src/profile/wtf_impl';
 
 @Component({
   selector: 'app-crew',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
+
+  memberBeingEdited: object = null;
+  inCrew: boolean = false;
 
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
@@ -16,6 +20,31 @@ export class CrewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    for(let i=0; i<this.crew.length; i++){
+      if(this.crew[i]['name']===memberName){
+        this.inCrew = true;
+      }
+    } if (!this.inCrew){
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+    this.inCrew = false;
+  }
+
+  edit(member:object) {
+    this.memberBeingEdited = member;
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
   }
 
 }
